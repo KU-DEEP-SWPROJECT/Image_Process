5258f8d7646700673ae6bb115d7da6474765ec42
GEONWOO0101 <woo1234656@gmail.com>
2023-11-10 13:33:17 +0900

    color_detect

--- test.py
+++ test.py
@@ -1,15 +1,33 @@
 import cv2
 
-image = cv2.imread('checking3.png')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-canny = cv2.Canny(gray, 120, 255, 1)
+# 색상 범위 설정
+lower_orange = (10, 100, 100)
+upper_orange = (20, 255, 255)
 
-corners = cv2.goodFeaturesToTrack(canny,4,0.5,50)
+lower_green = (40, 100, 100)
+upper_green = (80, 255, 255)
 
-for corner in corners:
-    x,y = corner.ravel()
-    #cv2.circle(image,(x,y),5,(36,255,12),-1)
-    print(x, y)
-cv2.imshow('canny', canny)
-cv2.imshow('image', image)
-cv2.waitKey()
+lower_blue = (90, 50, 50)
+upper_blue = (130, 255, 255)
+
+# 이미지 파일을 읽어온다
+img = cv2.imread("check_color.jpg", cv2.IMREAD_COLOR)
+
+# BGR to HSV 변환
+img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# 색상 범위를 제한하여 mask 생성
+mask_orange = cv2.inRange(img_hsv, lower_orange, upper_orange)
+mask_green = cv2.inRange(img_hsv, lower_green, upper_green)
+mask_blue = cv2.inRange(img_hsv, lower_blue, upper_blue)
+
+# 원본 이미지를 가지고 Object 추출 이미지로 생성
+result_orange = cv2.bitwise_and(img, img, mask=mask_orange)
+result_green = cv2.bitwise_and(img, img, mask=mask_green)
+result_blue = cv2.bitwise_and(img, img, mask=mask_blue)
+
+# 결과 이미지를 표시
+cv2.imshow("Orange", result_blue)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
