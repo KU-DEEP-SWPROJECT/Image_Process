8df6e36516a960b9d487bb047eb9c6b57b8b23b1
GEONWOO0101 <woo1234656@gmail.com>
2023-11-29 18:56:13 +0900

    aruco 수정

--- Gunwoo.py
+++ Gunwoo.py
@@ -1,42 +1,11 @@
 import numpy as np 
 import cv2 
 import math as mt
+from realsense_depth import *
+# from multiprocessing import Process, current_process, Array
 import cv2
-import pyrealsense2 as rs
+# import imutils
 
-class DepthCamera:
-    def __init__(self):
-        # Configure depth and color streams
-        self.pipeline = rs.pipeline()
-        config = rs.config()
-
-        # Get device product line for setting a supporting resolution
-        pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
-        pipeline_profile = config.resolve(pipeline_wrapper)
-        device = pipeline_profile.get_device()
-        device_product_line = str(device.get_info(rs.camera_info.product_line))
-
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
-
-
-
-        # Start streaming
-        self.pipeline.start(config)
-
-    def get_frame(self):
-        frames = self.pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-        if not depth_frame or not color_frame:
-            return False, None, None
-        return True, depth_image, color_image
-
-    def release(self):
-        self.pipeline.stop()
 
 loop_exit = 0
 
@@ -255,7 +224,7 @@ def mapping(k_real_size, turtle_num, burden_real_size, m_color):
     return bg_cnt, turtle_mapping_loc, burden_mapping_loc
 
 def get_points(loop_exit):
-    Aruco_Dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    Aruco_Dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
     goal_l = np.zeros((16,2), dtype=int)
     goal_cnt = np.zeros(4)
 
@@ -358,10 +327,10 @@ def get_points(loop_exit):
 
         
     ############################################
-        Aruco_Params = cv2.aruco.DetectorParameters()   
-        detector = cv2.aruco.ArucoDetector(Aruco_Dict, Aruco_Params)
-        (marker_corners, marker_id, rejected_markers) = detector.detectMarkers(aruco_image)
-        
+        Aruco_Params = cv2.aruco.DetectorParameters_create()
+        (marker_corners, marker_id, rejected_markers) = cv2.aruco.detectMarkers(
+            aruco_image, Aruco_Dict, parameters=Aruco_Params
+        )
         if len(marker_corners) > 0:
             marker_id = marker_id.flatten()
             ang = np.empty((0,2), dtype=int)
