f75bb103238bea828afe9b9d69695c9b0eb1e37e
GEONWOO0101 <woo1234656@gmail.com>
2023-11-29 17:57:29 +0900

    카메라 테스트

---
+++ cameara_test.py
@@ -0,0 +1,48 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+class DepthCamera:
+    def __init__(self):
+        # Configure depth and color streams
+        self.pipeline = rs.pipeline()
+        config = rs.config()
+
+        # Get device product line for setting a supporting resolution
+        pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        pipeline_profile = config.resolve(pipeline_wrapper)
+        device = pipeline_profile.get_device()
+        device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+
+
+
+        # Start streaming
+        self.pipeline.start(config)
+
+    def get_frame(self):
+        frames = self.pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+        if not depth_frame or not color_frame:
+            return False, None, None
+        return True, depth_image, color_image
+
+    def release(self):
+        self.pipeline.stop()
+    
+webcam = DepthCamera()
+while(1):
+    _ ,_,imageFrame= webcam.get_frame()
+    imageFrame = imageFrame[:,200:1000]
+    cv2.imshow("test", imageFrame)
+    
+    if cv2.waitKey(10) & 0xFF == ord('q'): 
+            webcam.release() 
+            cv2.destroyAllWindows() 
+            break
